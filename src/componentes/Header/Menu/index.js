03cc3ee6b4be300058a49f4a4cf676e6d631ea90
Cartol4 <vieira.cunha.miguel@escola.pr.gov.br>
2026-08-25 12:31:37 -0300

    Add files via upload

---
+++ src/componentes/Header/Menu/index.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import './index.css'
+
+function Menu(){
+    return (
+        <nav>
+            <ul>
+                <li><Link to = "/">Home</Link></li>
+                <li><Link to = "/contato">Contato</Link></li>
+                <li><Link to = "/sobre">Sobre</Link></li>
+            </ul>
+        </nav>
+    )
+}
+export default Menu;
---
+++ src/paginas/Sobre/index.js
@@ -0,0 +1,44 @@
+import './index.css'
+import foto from 'minha-foto.png':
+import
+
+function Sobre() {
+    return (
+        <main>
+            <header>
+                <h1>Sobre</h1>
+            </header>
+            <section>
+                <div className='fotoPerfil'>
+                    <img src="" />
+                    <p>Rafael Florindo</p>
+                </div>
+                <div>
+                    <article>
+                        <h2>Boxe</h2>
+                        <img src="" />
+                        <p className='descrição'>
+                            Gosto de lutar e sou competidor
+                        </p>
+                    </article>
+                    <article>
+                        <h2>Desenho</h2>
+                        <img src="" />
+                        <p className='descrição'>
+                            Gosto de fazer uns rabiscos
+                        </p>
+                    </article>
+                    <article>
+                        <h2>Música</h2>
+                        <img src="" />
+                        <p className='descrição'>
+                            Gosto de ouvir música pra toda e qualquer ocasião
+                        </p>
+                    </article>
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default Sobre;
